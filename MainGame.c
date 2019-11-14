74c7e4c54a9d1eb267986c741f9e1b9ceb63cbfd
Dubois Jean-Christophe <jchrdubois@gmail.com>
2019-11-14 14:55:39 +0100

    légères modifications d'affichage

--- MainGame.c
+++ MainGame.c
@@ -17,12 +17,16 @@ int main()
 			if (i == LARGEUR/2 && j == HAUTEUR/2)
 			{
 				grid[j][i].object=none;
-				grid[j][i].person=pac_man_right;
+				grid[j][i].person=pac_man_left;
 			}
+			else if (i == 0 || i == LARGEUR-1)
+				grid[j][i].object=wallH;
+			else if (j == 0 || j == HAUTEUR-1)
+				grid[j][i].object=wallL;
 			else
-			{
 				grid[j][i].object=ball;
-			}
+				
+				
 			afficheSquare(&grid[j][i]);
 		}
 		printf("\n");
